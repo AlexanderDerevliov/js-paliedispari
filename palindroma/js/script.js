39a60db4d0cbca150ea788c9c7e22718d79cf9b3
AlexanderDerevliov <alex.feelred@gmail.com>
2023-10-26 15:50:30 +0200

    added script to palindrom file

---
+++ palindroma/js/script.js
@@ -0,0 +1,42 @@
+// Chiedere all'utente di inserire una parola
+
+// creare la funzione che determina se quella parola è palindroma oppure non lo è
+
+
+// chiedo all'utente di inserire la parola
+let userWord = (prompt ("Inserici la tua parola e verifico se è palindroma oppure no!"));
+console.log(userWord);
+
+// la parola palindroma è uguale a quella inserita dall'utente?
+let palindromWord = parolaPalindroma (userWord);
+
+// se ha la sessa identità - stampo che la parola è palindroma
+if (palindromWord == userWord) {
+    console.log("La Parola è palindroma");
+
+// altrimenti stampo che la parola non è palindroma
+} else {
+    console.log("La Parola non è palindroma");
+}
+
+console.log(userWord);
+console.log(palindromWord)
+
+// la funzione creata per determinare se è o non è la parola palindroma
+
+
+function parolaPalindroma (string) {
+
+    // divido la stringa in singole lettere
+    const parolaSplit = string.split("");
+    console.log (parolaSplit);
+    
+    // inverto l'ordine delle lettere
+    const parolaInvert = parolaSplit.reverse();
+    console.log (parolaInvert);
+    
+    // unisco le lettere in una stringa
+    const parolaJoin = parolaInvert.join("");
+
+    return parolaJoin;
+}
